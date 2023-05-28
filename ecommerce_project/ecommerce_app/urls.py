606185fc341d8462e4174651cc373eca28ba8d89
ipaulaandreea <andreeapaulaivan@gmail.com>
2023-05-28 17:04:33 +0300

    wip views and templates

--- ecommerce_project/ecommerce_app/urls.py
+++ ecommerce_project/ecommerce_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns= [
 path("", views.StartingPageView.as_view(), name="starting-page"),
+path("product/<slug:slug>",views.SingleProductView.as_view(), name="product-detail-page"),
 
 ]
--- ecommerce_project/ecommerce_app/views.py
+++ ecommerce_project/ecommerce_app/views.py
@@ -2,14 +2,30 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views import View
+
 from .models import Category, Product, Address, Client, Order, OrderItem
 
 class StartingPageView(ListView):
     template_name="ecommerce_app/starting_page.html"
     model=Product
+    context_object_name="products"
 
     def get_queryset(self):
         queryset=super().get_queryset()
-        data=queryset[:3]
-        return data
+        return queryset
     #create pagination 
+
+class SingleProductView(View):
+ 
+    def get(self,request,slug):
+        product=Product.objects.get(slug=slug)
+        description=Product.objects.get(description=description)
+        price=Product.objects.get(price=price)
+        
+        context={
+            "product": product,
+            "description":description,
+            "price":price,
+        }
+        return render(request, "ecommerce_app/product-detail.html", context)
